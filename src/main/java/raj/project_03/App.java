30ffed973ba8f08be8d40ba29373eed4d054b6c8
Agnieszka Limiszewska <al59720@st.amu.edu.pl>
2015-01-26 00:10:43 +0100

    New repository

---
+++ src/main/java/raj/project_03/App.java
@@ -0,0 +1,13 @@
+package raj.project_03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
